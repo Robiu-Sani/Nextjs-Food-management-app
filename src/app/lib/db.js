9b94bff2c9b911c1c617e19f5950bc58904345e5
Robiu-Sani <hafazrobiussani@gmail.com>
2024-10-01 20:55:38 +0600

    testing

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,2 +1,3 @@
-const { username, password } = process.env;
+// ${process.env.DB_USER}:${process.env.DB_PASS}
+//slkdjflskdjfls
 export const connectiondb = `mongodb+srv://Lux-tower:EhBuhnQW6GJLSEX8@cluster0.541tyao.mongodb.net/Feast-Frame?retryWrites=true&w=majority&appName=Cluster0`;
